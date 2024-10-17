82c411d2e12a7a0295ee638d34a41cefa820cb1e
nich_otieno <nich.otieno@gmail.com>
2024-10-17 15:25:38 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h2>© 2021 The Brutal Martian LandScape</h2>
+        <h1>APOD Project</h1>
+      </div>
+    </footer>
+  );
+}
